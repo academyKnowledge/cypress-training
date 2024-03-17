08f33bb4e06ac6390ddbc6f6b838ff2a63a7b3ad
academyKnowledge <academyk74@gmail.com>
2024-03-17 06:14:32 +0200

    Cucumber + Javascript Basics

---
+++ cypress/e2e/cucumber/Tests/loginTest.feature
@@ -0,0 +1,9 @@
+@smokeTest @login @TC-1234
+Feature: login functionality
+
+    Scenario: Validate that the user can login using valid email and password
+        Given The user navigate to Sign in page in magento website
+        When The user type email in email input field
+        And The user type password in password input field
+        And The user click on sign in button
+        Then The user will redirected to My account page
---
+++ cypress/javaScript/javascript2.js
@@ -0,0 +1,107 @@
+// Variables
+
+// var , let , const 
+// var x = 1; 
+// var x = 2;   
+
+// const message = "hello"; 
+// let y = 5;
+
+// + , - , % , * 
+
+let x = 5 ; 
+let y = "6" ; 
+let z = x+y ; 
+// console.log(z);
+// console.log(y*x)
+// console.log(5**2)  // 5 * 5 
+// console.log(5**3)  // 5 * 5 * 5 
+// console.log(y%x)
+// let t = ++x ;  
+// console.log(t);
+// console.log(--y)
+
+// console.log(x+=y) // x = x+y
+
+// console.log(x==y) // only value 
+// console.log(x===y) // value + type 
+// console.log(x!=y) // 5 != 6 
+// console.log(x>y);
+// console.log(x<=y);
+
+let flag = true ; 
+
+// console.log(flag)
+// console.log(!flag) // not (true) = false 
+
+// let x1 = true ; 
+// let x2 = false ; 
+
+// console.log(x1 && x2); // true && false = false 
+// console.log(x1 ||  x2); // true && false = true  
+
+// console.log(typeof(flag))
+// console.log(typeof(x))
+// console.log(typeof(y))
+
+let number1 =  10 ; 
+
+// if(number1!=7){  // 5 !=7   -> true 
+//     console.log(number1)
+// }
+// else {
+//     console.log("The number is equal 7")
+// }
+
+// if(number1!=7 && number1!=10){  // 10 != 7 && 10!=10 
+//                                 // true && false  -> false 
+//     console.log(number1)
+// }
+// else if(number1>7){  // 10 > 7 -> true 
+//     console.log("The number is greater than 7")
+// }
+// else {
+//     console.log("The number is equal 7")
+// }
+
+
+// x+=y -> x = x+y  
+// numbers[0]+=5 
+// numbers[1]+=5 
+// numbers[2]+=5 
+// numbers[3]+=5 
+// numbers[4]+=5 
+
+// console.log(numbers)
+
+// loop - for loop , while loop , do while 
+// console.log(numbers.length)
+
+// for(let i =0; i<numbers.length; i++){
+//     numbers[i]+=5 
+// }
+ // 0 , 1 , 2 , 3 , 4 , 5 
+// console.log(numbers)
+
+// let numbers = [1,2,3,4,5, 8 , 10 , 12 , 30];
+// let numbers2 = [1,2,3, 30];
+
+// function addValueToArray(arr){
+//     for(let i =0; i<arr.length; i++){
+//         arr[i]+=5 
+//     }  
+// }
+
+// addValueToArray(numbers)
+
+// let numbers = [1,2,3,4,5, 8 , 10 , 12 , 30];
+
+// let i =0 ; 
+
+// while(i<numbers.length){
+//     numbers[i]+=5;
+    
+
+//     i++; 
+// }
+// console.log(numbers)
